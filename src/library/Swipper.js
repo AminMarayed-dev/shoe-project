264a082a41875ea9ef0de39c512b8a7bb81d80f7
AminMarayed <aminmarayed4@gmail.com>
2024-04-06 22:03:18 +0330

    feat: create swiper js

---
+++ src/library/Swipper.js
@@ -0,0 +1,17 @@
+
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+export const swiper = new Swiper(".swiper", {
+  loop:true,
+  
+  navigation: {
+    nextEl: ".next",
+  },
+
+  // scrollbar: {
+  //   el: ".next-scroll",
+  //   hide: false,
+  // },
+});
+
+
