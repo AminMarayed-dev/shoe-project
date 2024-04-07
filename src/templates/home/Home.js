e763bd72f288994d1163671895bff587f4f29322
AminMarayed <aminmarayed4@gmail.com>
2024-04-07 23:50:10 +0330

    update home page

--- src/templates/home/Home.js
+++ src/templates/home/Home.js
@@ -1,8 +1,15 @@
+import { Icon } from "@/components/icon/Icon";
+import { Input } from "@/components/input/Input";
+import { InputSearch } from "@/widget/input-search";
+import { Footer } from "@/layout/footer/Footer";
 import { El } from "@/utils/create-element";
+import { CategoriesLogo } from "@/widget/categories";
+import { Header } from "@/layout/header/Header";
+import { filterCategories } from "@/widget/filter-categories";
 
 export function HomePage() {
-    return El({
-        element:'div',
-        innerText:'home page'
-    })
+  return El({
+    element: "div",
+    children: [Header(),InputSearch(),CategoriesLogo(),filterCategories(), Footer()],
+  });
 }
