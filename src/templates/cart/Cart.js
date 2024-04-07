2a4f652c827e87e3a8f89b9a453851bfcdf199f4
AminMarayed <aminmarayed4@gmail.com>
2024-04-07 23:50:43 +0330

    update cart

--- src/templates/cart/Cart.js
+++ src/templates/cart/Cart.js
@@ -1,8 +1,9 @@
+import { Footer } from "@/layout/footer/Footer";
 import { El } from "@/utils/create-element";
 
 export function CartPage() {
     return El({
         element:'div',
-        innerText:'cart page'
+        children:[Footer()]
     })
 }
