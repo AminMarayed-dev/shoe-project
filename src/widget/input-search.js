b851d60056bd006690f645c9712e5803cac8a87a
AminMarayed <aminmarayed4@gmail.com>
2024-04-08 13:32:50 +0330

    Update input search widget

--- src/widget/input-search.js
+++ src/widget/input-search.js
@@ -5,6 +5,10 @@ import { Input } from "../components/input/Input";
 export function InputSearch() {
   return El({
     element: "div",
-    children: [Icon({}), Input({ type: "text", placeholder: "search" })],
+    className:'relative px-2',
+    children: [
+      Icon({ className: "icon-[ic--round-search]", otherClass:'absolute top-2 left-4 text-lg bg-gray-400' }),
+      Input({ type: "text", placeholder: "search", className:'bg-gray-100 px-7 py-1 w-full' }),
+    ],
   });
 }
