c125c539ab87fd352e92b653d3aba0cc8e8f7950
AminMarayed <aminmarayed4@gmail.com>
2024-04-08 21:29:55 +0330

    feat: add navigate route search in input widget

--- src/widget/input-search.js
+++ src/widget/input-search.js
@@ -1,6 +1,8 @@
 import { El } from "@/utils/create-element";
 import { Icon } from "../components/icon/Icon";
 import { Input } from "../components/input/Input";
+import { router } from "@/routes/routes";
+
 
 export function InputSearch() {
   return El({
@@ -10,5 +12,13 @@ export function InputSearch() {
       Icon({ className: "icon-[ic--round-search]", otherClass:'absolute top-2 left-4 text-lg bg-gray-400' }),
       Input({ type: "text", placeholder: "search", className:'bg-gray-100 px-7 py-1 w-full' }),
     ],
+    eventListener:[
+      {
+        event:'click',
+        callback: () => {
+          router.navigate('/search')
+        }
+      }
+    ]
   });
 }
