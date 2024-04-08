c21849375ec793fa5cbe8c69c7e21ef19a32d87e
AminMarayed <aminmarayed4@gmail.com>
2024-04-08 21:28:02 +0330

    update header layout

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,38 +1,57 @@
 import { Icon } from "@/components/icon/Icon";
+import { router } from "@/routes/routes";
 import { El } from "@/utils/create-element";
 
 export function Header() {
-    return El({
-        element:'div',
-        className:'flex justify-between',
-        children:[
-            El({
-                element:'span',
-                className:'flex',
-                children:[
-                    El({
-                        element:'img',
-                        alt:'header_img'
-                    }),
-                    El({
-                        element:'span',
-                        children:[
-                            El({
-                                element:'p',
-                                innerText:'Good Morning 👋'
-                            }),
-                            El({
-                                element:'p',
-                                innerText:'Saeed Abdilar'
-                            })
-                        ]
-                    })
-                ]
-            }),
-            El({
-                element:'span',
-                children:[Icon({}), Icon({})]
-            })
-        ]
-    })
+  return El({
+    element: "div",
+    className: "flex justify-between items-center px-4 py-1",
+    children: [
+      El({
+        element: "span",
+        className: "flex",
+        children: [
+          El({
+            element: "img",
+          }),
+          El({
+            element: "span",
+
+            children: [
+              El({
+                element: "p",
+                className: "text-gray-500",
+                innerText: "Good Morning 👋",
+              }),
+              El({
+                element: "p",
+                className: "font-bold",
+                innerText: "Saeed Abdilar",
+              }),
+            ],
+          }),
+        ],
+      }),
+      El({
+        element: "span",
+        className: "flex gap-2",
+        children: [
+          Icon({
+            className: "icon-[mdi--notifications-none]",
+            otherClass: "text-xl cursor-pointer",
+          }),
+          Icon({
+            className: "icon-[mdi--cards-heart-outline]",
+            otherClass: "text-xl cursor-pointer",
+            eventListener: [
+              {
+                event: "click",
+                callback: () => router.navigate("/wishlist"),
+              },
+            ],
+          }),
+        ],
+      }),
+    ],
+  });
 }
