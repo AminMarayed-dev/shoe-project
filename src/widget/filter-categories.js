734fc8c543ee580baa2a67d03f38688eaa0a2757
AminMarayed <aminmarayed4@gmail.com>
2024-04-08 21:31:04 +0330

    feat : add navigate route /most-popular in filter categories widget

--- src/widget/filter-categories.js
+++ src/widget/filter-categories.js
@@ -2,6 +2,7 @@ import { Button } from "@/components/button/Button";
 import { Text } from "@/components/text/Text";
 import { El } from "@/utils/create-element";
 import { filterCategoriesBtn } from "./filter-categories-button";
+import { router } from "@/routes/routes";
 
 export function filterCategories() {
   return El({
@@ -12,7 +13,12 @@ export function filterCategories() {
         className: "flex justify-between items-center mb-4",
         children: [
           Text({ text: "Most Popular", className: "font-bold text-[20px]" }),
-          Text({ text: "See All", className: "text-[16px] cursor-pointer" }),
+          Text({ text: "See All", className: "text-[16px] cursor-pointer", eventListener:[{
+            event:'click',
+            callback:() => {
+              router.navigate('/most-popular')
+            }
+          }] }),
         ],
       }),
       filterCategoriesBtn()
