6a7a3678e9b6fc9e296f40083011b1c10e1dbc26
AminMarayed <aminmarayed4@gmail.com>
2024-04-11 05:21:51 +0330

    Update router file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,28 +7,57 @@ import { Cart } from "@/pages/cart/Cart";
 import { Search } from "@/pages/search/Search";
 import { CheckOut } from "@/pages/checkout/CheckOut";
 import { Login } from "@/pages/login/Login";
+import { WishList } from "@/pages/wishlist/WishList";
+import { MostPopular } from "@/pages/most-popular/Most-Popular";
+import {
+  getProducts,
+  getProductsByFilter,
+  getProductsById,
+} from "@/api/Products";
+import { SingleProduct } from "@/pages/single-product/Single_Product";
+import { Brand } from "@/pages/brand/Brand";
 
 export const router = new Navigo("/");
 
 router
   .on("/", () => {
-    changePage(Slides);
+    changePage(Home());
   })
   .on("/home", () => {
-    changePage(Home);
+    changePage(Home());
   })
   .on("/orders", () => {
-    changePage(Orders);
+    changePage(Orders());
   })
   .on("/search", () => {
-    changePage(Search);
+    changePage(Search());
   })
   .on("/cart", () => {
-    changePage(Cart);
+    changePage(Cart());
   })
   .on("/checkout", () => {
-    changePage(CheckOut);
+    changePage(CheckOut());
   })
   .on("/login", () => {
-    changePage(Login);
+    changePage(Login());
+  })
+  .on("/most-popular", () => {
+    changePage(MostPopular());
+  })
+  .on("/wishlist", () => {
+    changePage(WishList());
+  })
+  .on("/products/:id", ({ data }) => {
+    // console.log(data);
+    getProductsById(data.id).then((response) => {
+      // console.log(response);
+      changePage(SingleProduct(response));
+    });
+  })
+  .on("/brand/:brand", ({ data }) => {
+    // console.log(data);
+    getProductsByFilter("brand", data.brand.toUpperCase()).then((response) => {
+      // console.log(response);
+      changePage(Brand(response));
+    });
   });
